e99b9cac2eed9a5149b51e1c98d4389869ad79a9
Nate Corkish <natecorkish@nates-mbp.home>
2020-08-26 17:42:25 +0100

    larger header title and padding after header button

--- source/_layouts/partials/header.blade.php
+++ source/_layouts/partials/header.blade.php
@@ -2,19 +2,19 @@
 
 	<!-- News image !-->
 	<div class="w-full lg:w-1/2 bg-primary border-r">
-		<img class="h-128 w-full" src="{{ $image ?? '' }}">
+		<img class="w-full" src="{{ $image ?? '' }}">
 	</div><!-- // News image !-->
 
 	<!-- Title and Description !-->
 	<div class="w-full lg:w-1/2 bg-primary p-4 md:p-0 sm:py-6  flex justify-start">
 		<div class="flex flex-col items-start justify-center md:px-10 pt-10">
-			<h2 class="text-2xl mt-2 font-bold mb-2 leading-tight text-darker">
+			<h2 class="text-4xl mt-2 font-bold mb-2 leading-tight text-darker">
 				{{ $title ?? '' }}
 			</h2>
 			<h3 class="text-sm font-semibold my-6 sm:mb-12 text-light">
 				{!! $description ?? '' !!}
 			</h3>
-			<div class="flex mb-6 lg:mb-0">
+			<div class="flex pb-6 lg:mb-0">
 				{{ $buttons ?? '' }}
 			</div>
 		</div>
